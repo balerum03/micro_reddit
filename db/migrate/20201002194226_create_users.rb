f08c04f3f4aee0151841fb8453532988a6026b5f
balerum03 <adafer123.angra@gmail.com>
2020-10-02 16:31:43 -0500

    made changes to the models

--- db/migrate/20201002194226_create_users.rb
+++ db/migrate/20201002194226_create_users.rb
@@ -1,10 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      username :string
-      email :string
-      password :string
-      has_many :posts
+      t.string :username, :unique => true
+      t.string :email, :unique => true
+      t.string :password, :nil => false, :length => {:within => 6..15}
       t.timestamps
     end
   end
--- db/migrate/20201002194730_create_posts.rb
+++ db/migrate/20201002194730_create_posts.rb
@@ -1,10 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      title :string
-      body :text
-      user_id :integer
-      belongs_to :user
+      t.string :tittle, :unique => true
+      t.text :body, :nil => false
+      t.integer :user_id, :unique => true
       t.timestamps
     end
   end
