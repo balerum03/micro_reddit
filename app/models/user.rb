d1e3e64ecd8aa40df9a8d33ca28db3adb5b285ce
balerum03 <adafer123.angra@gmail.com>
2020-10-05 12:38:21 -0500

    Added comments table and ran some test on it by adding data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
   validates :user_name, presence: true
   validates :email, presence: true
   validates :password, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_194730) do
+ActiveRecord::Schema.define(version: 2020_10_05_163341) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "tittle"
