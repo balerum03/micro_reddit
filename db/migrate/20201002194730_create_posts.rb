4bf59e87d35dd021e062e539d303dd7460d995a5
balerum03 <adafer123.angra@gmail.com>
2020-10-05 16:04:20 -0500

    validated title and body for post

--- db/migrate/20201002194730_create_posts.rb
+++ db/migrate/20201002194730_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :tittle, unique: true
+      t.string :title, unique: true
       t.text :body, nil: false
       t.integer :user_id, unique: true
       t.timestamps
